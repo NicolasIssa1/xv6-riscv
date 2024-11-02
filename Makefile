7aee7db8229e2376353d1155b205e74251665f65
Nicolas Issa <nicolasissa812@gmail.com>
2024-11-02 21:39:58 +0000

    m

--- Makefile
+++ Makefile
@@ -139,6 +139,8 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_hw\
+        $U/_my_shell
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/hw.c
@@ -0,0 +1,7 @@
+#include "/uolstore/home/users/sc23ni/comp2211/xv6_riscv_comp2211/kernel/types.h"
+#include "/uolstore/home/users/sc23ni/comp2211/xv6_riscv_comp2211/user/user.h"
+
+int main(int argc, char *argv[]) {
+    printf("Hello world\n");
+    exit(0);
+}
